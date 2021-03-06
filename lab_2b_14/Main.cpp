41d94dc89e275ff1c83dd28768bca94ea00c32b9
yar.drovolsky@gmail.com <yar.drovolsky@gmail.com>
2021-03-06 23:56:45 +0200

    lab_2b_14

---
+++ lab_2b_14/Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+//using namespace std;
+
+struct ListNode {
+	ListNode* prev;
+	ListNode* next;
+	int data;
+};
+
+struct PriorityQueue {
+	ListNode* head;
+	ListNode* end;
+	std::size_t size;
+};
+
+int main() {
+
+
+	std::system("pause");
+	return 0;
+}
