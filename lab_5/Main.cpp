ab4c97a3e41e3df793a424d97045c63e688b814b
YarikDrovolsky <yar.drovolsky@gmail.com>
2021-04-26 11:32:55 +0300

    Implemented adjacency matrix for graph: add, remove edge, print matrix, print edges
    Direction of edge is available (if edge between vertex i and j is bidirectional, so at fact we have to edges: i->j and j->i)
    Weight of edge is available

--- lab_5/Main.cpp
+++ lab_5/Main.cpp
@@ -1,7 +1,85 @@
 #include <iostream>
+#include <cassert>
+
+struct AdjMatrix {
+	int** matrix;
+	std::size_t size;
+
+	AdjMatrix(std::size_t size) {
+		this->size = size;
+		this->matrix = new int* [size];
+
+		for (std::size_t i = 0; i < size; i++) {
+			this->matrix[i] = new int[size];
+			for (std::size_t j = 0; j < size; j++) {
+				this->matrix[i][j] = 0;
+			}
+		}
+	}
+
+	void add_edge(std::size_t start, std::size_t end, int weight) {
+		assert(start < this->size);
+		assert(end < this->size);
+		this->matrix[start][end] = weight;
+	}
+
+	void remove_edge(std::size_t start, std::size_t end) {
+		assert(start < this->size);
+		assert(end < this->size);
+		this->matrix[start][end] = 0;
+	}
+	void print_matrix() {
+		for (std::size_t i = 0; i < size; i++) {
+			for (std::size_t j = 0; j < size; j++) {
+				std::cout << this->matrix[i][j] << " ";
+			}
+			std::cout << std::endl;
+		}
+	}
+
+	void print_edges() {
+		bool is_empty = true;
+		for (std::size_t i = 0; i < size; i++) {
+			bool is_edge = false;
+			for (std::size_t j = 0; j < size; j++) {
+				if (this->matrix[i][j] != 0) {
+					std::cout << i << "->" << j << " (" << this->matrix[i][j] << "), ";
+					is_edge = true;
+					is_empty = false;
+				}
+			}
+			if (is_edge) {
+				std::cout << std::endl;
+			}
+			
+		}
+		if (is_empty) {
+			std::cout << "No edges" << std::endl;
+		}
+	}
+
+	~AdjMatrix() {
+		this->size = 0;
+		for (std::size_t i = 0; i < size; i++) {
+			delete[]this->matrix[i];
+		}
+		delete []this->matrix;
+	}
+
+
+};
+
 
 int main() {
-
+	AdjMatrix graph1(5);
+	//graph1.add_edge(1, 2, 5);
+	graph1.add_edge(1, 3, 5);
+	graph1.print_matrix();
+	graph1.print_edges();
+	std::cout << "\nRemove edge 1->3:\n";
+	graph1.remove_edge(1, 3);
+	graph1.print_matrix();
+	graph1.print_edges();
 
 
 
